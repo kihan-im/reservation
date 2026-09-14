905fbed4a051055c9873756a0a4dff1ddc050cd9
kimss <sangsoo.kim@nts-corp.com>
2026-09-14 15:33:27 +0900

    chore: 불필요한 오타 방지용 배치 파일(register_schdule.bat, unregister_schdule.bat) 삭제 및 폴더 정리

--- register_schdule.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM 파일명 철자 오타(register_schdule vs register_scheduler) 자동 지원
-call "%~dp0register_scheduler.bat" %*
--- unregister_schdule.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM 파일명 철자 오타(unregister_schdule vs unregister_scheduler) 자동 지원
-call "%~dp0unregister_scheduler.bat" %*
